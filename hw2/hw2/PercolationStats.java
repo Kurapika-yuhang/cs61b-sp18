08c7eda61a3449171fc7e8c6771d7529380f5077
Yuhang Zhao <zhaoyuhang0126@qq.com>
2021-07-06 18:12:25 +0800

    HW2 finished 21/07/06 style fixed 置信区间*/顺序改变

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -10,6 +10,10 @@ public class PercolationStats {
 
     public PercolationStats(int N, int T, PercolationFactory pf) {
         // perform T independent experiments on an N-by-N grid
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+
         thresholds = new double[T];
         for (int i = 0; i < T; i++) {
             // 一次实验 算出一个threshold的值
@@ -35,11 +39,12 @@ public class PercolationStats {
 
     public double confidenceLow() {
         // low endpoint of 95% confidence interval
-        return mean() - 1.96 / Math.sqrt(T) * stddev();
+        //return mean() - 1.96 / Math.sqrt(T) * stddev();
+        return mean() - 1.96 * stddev() / Math.sqrt(T) ; // 先乘后除和先除后乘的区别？
     }
 
     public double confidenceHigh() {
         // high endpoint of 95% confidence interval
-        return mean() + 1.96 / Math.sqrt(T) * stddev();
+        return mean() + 1.96 * stddev() / Math.sqrt(T) ;
     }
 }
