6e5efca6030f27b2b497ff0397cb4444ed1b3c46
Yuhang Zhao <zhaoyuhang0126@qq.com>
2021-07-06 17:39:02 +0800

    HW2 finished 21/07/06

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,123 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import java.lang.IndexOutOfBoundsException;
+import java.lang.IllegalArgumentException;
 
 public class Percolation {
+    private int N;
+    private boolean[][] openFlag;
+    private int numOfOpenSites;
+    private WeightedQuickUnionUF sites; // 记录是不是full
+    private WeightedQuickUnionUF sitesWithoutBottom; // 不记录bottom的union 防止backwash
+    private int top;
+    private int bottom;
 
+    public Percolation(int N) {
+        // create N-by-N grid, with all sites initially blocked
+
+        this.N = N;
+        top = N*N;
+        bottom = N*N + 1;
+        numOfOpenSites = 0;
+
+        // openFlag initialization：all false
+        openFlag = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                openFlag[i][j] = false;
+            }
+        }
+
+        // sites initialization: 第一行与top连，最后一行与bottom连 （有backwash 只为了方便isfull（）为const time）
+        sites = new WeightedQuickUnionUF(N * N + 2);
+        for (int i = 0; i < N; i++) {
+            sites.union(top,xyTo1D(0, i));
+        }
+
+        for (int i = 0; i < N; i++) {
+            sites.union(bottom,xyTo1D(N - 1, i));
+        }
+
+        // sitesWithoutBottom initialization: 第一行与top连 没有bottom（防止backwash）
+        sitesWithoutBottom = new WeightedQuickUnionUF(N * N + 1);
+        for (int i = 0; i < N; i++) {
+            sitesWithoutBottom.union(top,xyTo1D(0, i));
+        }
+    }
+
+
+
+
+    public boolean isFull(int row, int col) {
+        // is the site (row, col) full?
+        validate(row, col);
+        return sitesWithoutBottom.connected(top, xyTo1D(row, col));
+    }
+    public int numberOfOpenSites() {
+        // number of open sites
+        return numOfOpenSites;
+    }
+    public boolean percolates() {
+        // does the system percolate?
+        return sites.connected(top, bottom);
+    }
+
+    public void open(int row, int col) {
+        // open the site (row, col) if it is not open already
+        validate(row, col);
+        if (isOpen(row, col)) {
+            return;
+        }
+
+        openFlag[row][col] = true;
+        numOfOpenSites += 1;
+        unionAround(row, col);
+    }
+
+    public boolean isOpen(int row, int col) {
+        // is the site (row, col) open?
+        validate(row, col);
+        return openFlag[row][col];
+    }
+
+
+    private void unionAround(int row, int col) {
+        validate(row, col);
+        int[][] dirArray = new int[][] {{0,1},{0,-1},{1,0},{-1,0}};
+        for (int[] dir: dirArray) {
+            int rowAround = row + dir[0];
+            int colAround = col + dir[1];
+            if (isValidRange(rowAround,colAround) && isOpen(rowAround,colAround)) {
+                sites.union(xyTo1D(row,col), xyTo1D(rowAround, colAround));
+                sitesWithoutBottom.union(xyTo1D(row,col), xyTo1D(rowAround, colAround));
+            }
+        }
+    }
+
+    private int xyTo1D(int row, int col) {
+        return N * row + col;
+    }
+
+    private boolean isValidRange(int row, int col) {
+        return row >= 0 && row < N && col >= 0 && col < N;
+    }
+
+    private void validate(int row, int col) {
+        if (row < 0 || col < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (row >= N || col >= N) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public static void main(String[] args) {
+        // use for unit testing (not required)
+        Percolation percolation = new Percolation(2);
+        System.out.println(percolation.percolates());  // false
+        percolation.open(0,0);
+        percolation.open(1,0);
+        System.out.println(percolation.percolates());  // true
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,47 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+import java.lang.IllegalArgumentException;
+
+public class PercolationStats {
+    private int N;  // N-by-N grid
+    private int T;  // T independent experiments
+    private double[] thresholds;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        // perform T independent experiments on an N-by-N grid
+        thresholds = new double[T];
+        for (int i = 0; i < T; i++) {
+            // 一次实验 算出一个threshold的值
+            Percolation p = pf.make(N);
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(0,N);
+                int col = StdRandom.uniform(0,N);
+                p.open(row, col);
+            }
+            thresholds[i] = (double) p.numberOfOpenSites()/(N*N);
+        }
+    }
+
+    public double mean() {
+        // sample mean of percolation threshold
+        return StdStats.mean(thresholds);
+    }
+
+    public double stddev() {
+        // sample standard deviation of percolation threshold
+        return StdStats.stddev(thresholds);
+    }
+
+    public double confidenceLow() {
+        // low endpoint of 95% confidence interval
+        return mean() - 1.96 / Math.sqrt(T) * stddev();
+    }
+
+    public double confidenceHigh() {
+        // high endpoint of 95% confidence interval
+        return mean() + 1.96 / Math.sqrt(T) * stddev();
+    }
 }
