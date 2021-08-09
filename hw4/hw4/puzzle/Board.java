57fe5336ef0ffe0c54beba4b619092578fcbd054
Yuhang Zhao <zhaoyuhang0126@qq.com>
2021-08-09 15:11:37 +0800

    HW4 finished

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,10 +1,144 @@
 package hw4.puzzle;
 
-public class Board {
+import java.util.LinkedList;
+import java.util.List;
+
+public class Board implements WorldState {
 
     /** Returns the string representation of the board. 
       * Uncomment this method. */
-    /*public String toString() {
+    private int[][] tiles;
+    private static final int BLANK = 0;
+    private int N;
+
+    public Board(int[][] tiles) {
+        N = tiles.length;
+        this.tiles = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                this.tiles[i][j] = tiles[i][j];
+            }
+        }
+    }
+
+    public int tileAt(int i, int j) {
+        if (i < 0 || i >= size() || j < 0 || j >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        return tiles[i][j];
+    }
+
+    public int size() {
+        return N;
+    }
+
+    @Override
+    public Iterable<WorldState> neighbors() {
+        List<WorldState> neighbors = new LinkedList<>();
+
+        int N = size();
+        int[] blankIdx = findBlank();
+        List<int[]> neighborIdx = findNeighborIdx(blankIdx);
+
+        int[][] newBoard = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                newBoard[i][j] = tileAt(i, j);
+            }
+        }
+
+        for (int[] idx:neighborIdx) {
+            newBoard[blankIdx[0]][blankIdx[1]] = newBoard[idx[0]][idx[1]];
+            newBoard[idx[0]][idx[1]] = BLANK;
+            neighbors.add(new Board(newBoard));
+            newBoard[idx[0]][idx[1]] = newBoard[blankIdx[0]][blankIdx[1]];
+            newBoard[blankIdx[0]][blankIdx[1]] = BLANK;
+        }
+        return neighbors;
+    }
+
+    private List<int[]> findNeighborIdx(int[] blankIdx) {
+        /* return a list of the idx of blank's neighbors */
+        List<int[]> neighbors = new LinkedList<>();
+        int[][] moves = new int[][] {{0,1},{1,0},{0,-1},{-1,0}};
+        for (int[] move:moves) {
+            int row = blankIdx[0] + move[0];
+            int col = blankIdx[1] + move[1];
+            if (0 <= row && row <= size() - 1 && col >= 0 && col <= size() - 1) {
+                neighbors.add(new int[]{row, col});
+            }
+        }
+        return neighbors;
+    }
+
+    private int[] findBlank() {
+        int[] res = new int[2];
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < size(); j++) {
+                if (tiles[i][j] == BLANK) {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        throw new IndexOutOfBoundsException("No BLANK!");
+    }
+
+
+
+    public int hamming() {
+        int N = size();
+        int res = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tileAt(i, j) != i * N + j + 1) {
+                    res += 1;
+                }
+            }
+        }
+        return res - 1;
+    }
+
+    public int manhattan() {
+        int N = size();
+        int res = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int value = tileAt(i, j);
+                if (value == BLANK) {
+                    continue;
+                }
+                int rowGoal = (value - 1) / N;
+                int colGoal = (value - 1) % N;
+                res += Math.abs(rowGoal - i) + Math.abs(colGoal - j);
+            }
+        }
+        return res;
+    }
+
+    public boolean equals(Object y) {
+
+        if (y == null || !y.getClass().equals(this.getClass())) {
+            return false;
+        }
+
+        Board board = (Board) y;
+        if (board.size() != this.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < size(); j++) {
+                if (this.tileAt(i, j) != board.tileAt(i, j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
@@ -16,6 +150,10 @@ public class Board {
         }
         s.append("\n");
         return s.toString();
-    }*/
+    }
 
+    @Override
+    public int estimatedDistanceToGoal() {
+        return manhattan();
+    }
 }
---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,106 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.*;
+
+public class Solver {
+
+    private List<WorldState> solution;
+    private HashMap<WorldState, Integer> expMap;
+
+    public class SearchNode {
+
+        private WorldState state;
+        private int moves;  // the number of moves made to reach this world state from the initial state.
+        private SearchNode prev;    // a reference to the previous search node.
+
+        public SearchNode(WorldState state, int moves, SearchNode prev) {
+            this.state = state;
+            this.moves = moves;
+            this.prev = prev;
+        }
+    }
+
+    public class SearchNodeComparator implements Comparator<SearchNode> {
+
+        @Override
+        public int compare(SearchNode o1, SearchNode o2) {
+            int exp1 = getExpDist(o1.state);
+            int exp2 = getExpDist(o2.state);
+            int priority1 = exp1 + o1.moves;
+            int priority2 = exp2 + o2.moves;
+
+            return priority1 - priority2;
+        }
+    }
+
+    public Solver(WorldState initial) {
+        /*
+        * Constructor which solves the puzzle, computing
+                 everything necessary for moves() and solution() to
+                 not have to solve the problem again. Solves the
+                 puzzle using the A* algorithm. Assumes a solution exists.
+        * */
+        solution = new LinkedList<>();
+        expMap = new HashMap<>();
+
+        SearchNode currNode = new SearchNode(initial, 0, null);
+        MinPQ<SearchNode> pq = new MinPQ<>(new SearchNodeComparator());
+
+        pq.insert(currNode);
+
+        while (!pq.isEmpty()) {
+            currNode = pq.delMin();
+            // if find the goal, end.
+            if (currNode.state.isGoal()) {
+                break;
+            }
+
+            // explore each neighbor state
+            for(WorldState state: currNode.state.neighbors()) {
+                SearchNode node = new SearchNode(state, currNode.moves + 1, currNode);
+                // critical optimization
+                if (currNode.prev != null && state.equals(currNode.prev.state)) {
+                    // skip and go to the next neighbor
+                    continue;
+                }
+                pq.insert(node);
+            }
+        }
+
+        // record solution
+        Stack<WorldState> stateStack = new Stack<>();
+        for (SearchNode n = currNode; n != null; n = n.prev) {
+            stateStack.push(n.state);
+        }
+
+        while (!stateStack.isEmpty()) {
+            solution.add(stateStack.pop());
+        }
+    }
+
+    private int getExpDist(WorldState state) {
+        /* get the expected distance from the given state to the goal with caching */
+        if (!expMap.containsKey(state)) {
+            expMap.put(state, state.estimatedDistanceToGoal());
+        }
+        return expMap.get(state);
+    }
+
+    public int moves() {
+        /**
+         * Returns the minimum number of moves to solve the puzzle starting
+         * at the initial WorldState.
+         * */
+        return solution.size() - 1;
+    }
+
+    public Iterable<WorldState> solution() {
+        /*
+        * Returns a sequence of WorldStates from the initial WorldState
+                 to the solution.
+        * */
+        return solution;
+    }
+}
